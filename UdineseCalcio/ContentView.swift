47cdda0ae8e5b70f845c23967da55638c4971173
Romesh Singhabahu <romesh@earltech.it>
2020-12-17 16:26:04 +0100

    Initial Commit

---
+++ UdineseCalcio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UdineseCalcio
+//
+//  Created by Romesh Singhabahu on 17/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UdineseCalcio/UdineseCalcioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdineseCalcioApp.swift
+//  UdineseCalcio
+//
+//  Created by Romesh Singhabahu on 17/12/20.
+//
+
+import SwiftUI
+
+@main
+struct UdineseCalcioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
